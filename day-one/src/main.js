466209c1cf4bdc5848cd81e604ac992c1c6142f0
Luiiiz <52682525+meLuiiiz@users.noreply.github.com>
2021-08-17 19:14:53 -0300

    Criando função para alternar visibilidade do app

--- day-one/src/main.js
+++ day-one/src/main.js
@@ -1,6 +1,11 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('[data-js=app]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+document.querySelector('a').addEventListener('click', (event) => {
+  event.preventDefault()
+  document.querySelector('[data-js=app]').classList.toggle('hidden')
+})
